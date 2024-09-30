94156926aa9252c17b0bf53aa6b0d9293a1796e8
Conny <pedalconny@hotmail.com>
2024-09-30 11:52:26 +0200

    Add class QuantityDiscount

---
+++ src/main/java/org/example/QuantityDiscount.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class QuantityDiscount extends BaseDiscount {
+    public QuantityDiscount(Discount nextDiscount) {
+        super(nextDiscount);
+    }
+
+    @Override
+    protected boolean isApplicable(Product product) {
+        return product.getQuantity() >= 5;
+    }
+
+    @Override
+    protected double calculateDiscount(Product product) {
+        return 10.0;
+    }
+}
