e7224799c432b1bf1b1e9eb55d9759716426ce78
Conny <pedalconny@hotmail.com>
2024-09-30 11:16:51 +0200

    Add class FridayDiscount

---
+++ src/main/java/org/example/FridayDiscount.java
@@ -0,0 +1,19 @@
+package org.example;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class FridayDiscount extends BaseDiscount {
+    public FridayDiscount(Discount discount) {
+        super(discount);
+    }
+
+    @Override
+    protected boolean isApplicable(Product product) {
+        return LocalDate.now().getDayOfWeek() == DayOfWeek.FRIDAY;
+    }
+
+    @Override
+    protected double calculateDiscount(Product product) {
+        return product.getPrice() * 0.10;
+    }
+}
